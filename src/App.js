9a50496a3dbdb03a9bb66da35a7827448a2185bb
Joshua Halsey <joshuahalsey@MacBook-Air.local>
2020-05-17 20:32:38 -0500

    initial build

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import axios from "axios";
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Educate App</h1>
+        <button>LOGIN</button>
+        <button>SIGN UP</button>
+      </div>
+    );
+    }
+}
+export default App;
