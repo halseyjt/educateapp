a3db97ff614a6fba8c343ab42bad1271b78ce906
Joshua Halsey <joshuahalsey@MacBook-Air.local>
2020-06-01 15:41:25 -0500

    changed model folder name to DB

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Get started
+
+ - install webpack via npm
+ - npm run dev - starts webpack 
+ - npm start - starts server/connects to DB
--- server.js
+++ server.js
@@ -1,4 +1,14 @@
+
 const express = require('express');
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
+require("dotenv").config();
+
+
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once("open", () => console.log("Connected to DB"));
+
+app.listen(port, () => console.log(`listening from port: ${port}`));
